bc0b52a768a6cea915a4f42b0b29726c51f6d72e
jeon su jung <sj1910924@icloud.com>
2023-06-15 02:34:56 +0900

    feat: Add functionality to separate working and done todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,34 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+  const workingTodos = todos.filter((todo) => !todo.isDone);
+  const doneTodos = todos.filter((todo) => todo.isDone);
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          toggleTodo={toggleTodo}
-          deleteTodo={deleteTodo}
-        />
-      ))}
-    </ul>
+    <div>
+      <h2>Working.. 🔥</h2>
+      <ul>
+        {workingTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+      <h2>Done..! 🎉</h2>
+      <ul>
+        {doneTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
